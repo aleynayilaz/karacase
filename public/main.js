d8e447123b24a4036f00a2926063c18dca8473a0
Aleyna <79020089+aleynayilaz@users.noreply.github.com>
2023-04-06 08:51:23 +0300

    first commit

---
+++ public/main.js
@@ -0,0 +1,19 @@
+
+function myFunctionToggle() {
+  var x = document.getElementById("toggle");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myFunctionToggle2() {
+  var x = document.getElementById("toggle2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
